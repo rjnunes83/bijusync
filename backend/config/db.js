3d966f25232caa24907ea857815141381ba75f0e
Rodrigo Nunes <rodrigonunes@MacBook-Air-de-Rodrigo.local>
2025-07-27 21:07:30 +0100

    Corrige dependências e atualiza package-lock para Deploy Render 15

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
     dialect: 'postgres',
     port: process.env.DB_PORT || 5432,
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 );
 
