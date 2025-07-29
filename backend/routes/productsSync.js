2def971a0c615cd6773e64d8aadd33ede0e846d1
Rodrigo Nunes <rodrigonunes@MacBook-Air-de-Rodrigo.local>
2025-07-29 11:27:16 +0100

    Convertido para ES Modules e prontos para deploy 3

--- backend/routes/productsSync.js
+++ backend/routes/productsSync.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import shopService from '../services/shopService.js';
-import { getProductsFromMainStore, createProductInStore } from '../services/shopifyService.js';
+import shopService from '../services/shopify/shopService.js';
+import { getProductsFromMainStore, createProductInStore } from '../services/shopify/shopifyService.js';
 
 const router = express.Router();
 
