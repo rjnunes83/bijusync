6f46eddd60df05e71a0e68786c6867de21694f0d
Rodrigo Nunes <rodrigonunes@MacBook-Air-de-Rodrigo.local>
2025-07-30 23:08:26 +0100

    repositorio novo junto 12

--- apps/frontend/app/shopify.server.ts
+++ apps/frontend/app/shopify.server.ts
@@ -52,4 +52,3 @@ export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
 export const login = shopify.login;
 export const registerWebhooks = shopify.registerWebhooks;
-export const sessionStorage = shopify.sessionStorage;
