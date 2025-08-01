3e1c602ab520dfe760f37f5aca55c3be5043ad4d
Rodrigo Nunes <rodrigonunes@MacBook-Air-de-Rodrigo.local>
2025-08-02 00:57:38 +0100

    fix(frontend): remove conflicting routes.ts to restore Remix routing 1

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -5,9 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals({ nativeFetch: true });
 
-// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
-// stop passing in HOST, so we can remove this workaround after the next major release.
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -46,21 +43,13 @@ export default defineConfig({
     port: Number(process.env.PORT || 3000),
     hmr: hmrConfig,
     fs: {
-      // See https://vitejs.dev/config/server-options.html#server-fs-allow for more information
       allow: ["app", "node_modules"],
     },
   },
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-        v3_singleFetch: false,
-        v3_routeConfig: true,
-      },
+      // future: { ... } foi removido para evitar o erro!
     }),
     tsconfigPaths(),
   ],
